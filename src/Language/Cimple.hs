50712489f9bfe17d183fa1359cb7f762a750e244
Restyled.io <commits@restyled.io>
2021-12-28 14:31:29 +0000

    Restyled by stylish-haskell

--- src/Language/Cimple.hs
+++ src/Language/Cimple.hs
@@ -7,8 +7,8 @@ module Language.Cimple
 import           Control.Monad.State.Lazy    (State)
 import           Data.Text                   (Text)
 
-import           Language.Cimple.AST         as X
 import           Language.Cimple.Annot       as X
+import           Language.Cimple.AST         as X
 import           Language.Cimple.Lexer       as X
 import           Language.Cimple.Parser      as X
 import           Language.Cimple.Tokens      as X
